957058555d484919540fffbafb84e51486aa5cf5
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-08-13 15:55:35 +0300

    artuh 20hv

--- app/js/custom.js
+++ app/js/custom.js
@@ -13,8 +13,20 @@ document.addEventListener("DOMContentLoaded", function () {
   renderer.setClearColor(0x999999);
   var scene = new THREE.Scene();
   var group = new THREE.Group();
-  scene.add(group);
+  scene.add(group); // Line Init
+
+  var material = new THREE.LineBasicMaterial({
+    color: 0xff0000
+  });
+  var geometry = new THREE.Geometry();
+  var line = new THREE.Line(geometry, material);
+  var vector1 = new THREE.Vector3(-100, 0, 0);
+  var vector2 = new THREE.Vector3(100, 0, 0);
+  geometry.vertices.push(vector1);
+  geometry.vertices.push(vector2);
+  line.rotation.z = Math.random() * Math.PI;
+  group.add(line);
   var camera = new THREE.PerspectiveCamera(40, width / height, 1, 1000);
-  camera.position.set(0, 0.3);
+  camera.position.set(0, 0, 300);
   renderer.render(scene, camera);
 });
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -14,11 +14,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
   let scene = new THREE.Scene();
   let group = new THREE.Group();
-
   scene.add(group);
 
+  // Line Init
+  let material = new THREE.LineBasicMaterial({
+    color: 0xff0000
+  });
+
+  let geometry = new THREE.Geometry();
+  let line = new THREE.Line(geometry, material);
+
+  let vector1 = new THREE.Vector3(-100, 0, 0);
+  let vector2 = new THREE.Vector3(100, 0, 0);
+
+  geometry.vertices.push(vector1);
+  geometry.vertices.push(vector2);
+
+  line.rotation.z = Math.random() * Math.PI;
+
+  group.add(line);
+
   let camera = new THREE.PerspectiveCamera(40, width / height, 1, 1000);
-  camera.position.set(0, 0.3);
+  camera.position.set(0, 0, 300);
 
   renderer.render(scene, camera);
 });
