1f4a881ff1051a90fb1664f05f91c3acd5591a53
Oleksandr Timoshchuk <o.timoshuk@gmail.com>
2019-08-13 14:38:43 +0300

    start lines Artuh

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,21 +1,20 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-  var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-  camera.position.set(0, 0, 100);
-  camera.lookAt(0, 0, 0);
-  var scene = new THREE.Scene();
-  var material = new THREE.LineBasicMaterial({
-    color: 0x0000ff
+  var canvas = document.getElementById("myscene"),
+      width = 600,
+      height = 600;
+  var renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
   });
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
-  geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-  geometry.vertices.push(new THREE.Vector3(10, 0, 0));
-  var line = new THREE.Line(geometry, material);
-  scene.add(line);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  renderer.setSize(width, height);
+  renderer.setClearColor(0x999999);
+  var scene = new THREE.Scene();
+  var group = new THREE.Group();
+  scene.add(group);
+  var camera = new THREE.PerspectiveCamera(40, width / height, 1, 1000);
+  camera.position.set(0, 0.3);
   renderer.render(scene, camera);
 });
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  let canvas = document.getElementById("myscene"),
+    width = 600,
+    height = 600;
 
-  var camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    1,
-    500
-  );
-  camera.position.set(0, 0, 100);
-  camera.lookAt(0, 0, 0);
+  let renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+  });
 
-  var scene = new THREE.Scene();
-  var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
-  geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-  geometry.vertices.push(new THREE.Vector3(10, 0, 0));
-  var line = new THREE.Line(geometry, material);
-  scene.add(line);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  renderer.setSize(width, height);
+  renderer.setClearColor(0x999999);
+
+  let scene = new THREE.Scene();
+  let group = new THREE.Group();
+
+  scene.add(group);
+
+  let camera = new THREE.PerspectiveCamera(40, width / height, 1, 1000);
+  camera.position.set(0, 0.3);
+
   renderer.render(scene, camera);
 });
